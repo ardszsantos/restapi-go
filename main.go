52e61122cfe7f8f3a933c016e176c8d06155075c
ardszsantos <lokgamer01@gmail.com>
2024-12-06 15:46:34 -0300

    pizzas struct

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type Pizza struct {
+	ID    int
+	nome  string
+	preco float64
+}
+
 func main() {
-	fmt.Println("Teste")
+
+	var pizzas = []Pizza{
+		{ID: 1, nome: "Toscana", preco: 49.5},
+		{ID: 2, nome: "Marguerita", preco: 79.5},
+		{ID: 3, nome: "Atum com queijo", preco: 69.5},
+	}
+
+	fmt.Println(pizzas)
 }
