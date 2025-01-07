f9ed5aaca63cfa8f70879ce4d687658569020a58
ardszsantos <lokgamer01@gmail.com>
2025-01-07 11:47:22 -0300

    .

--- main.go
+++ main.go
@@ -1,32 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"restAPI-go/models"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Pizza struct {
-	ID    int
-	nome  string
-	preco float64
-}
-
 func main() {
 
 	router := gin.Default()
-	router.GET("/pizzas", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"pizzas": "pizzas, pizas, pisas",
-		})
-	})
+	router.GET("/pizzas", getPizzas)
 
-	var pizzas = []Pizza{
-		{ID: 1, nome: "Toscana", preco: 49.5},
-		{ID: 2, nome: "Marguerita", preco: 79.5},
-		{ID: 3, nome: "Atum com queijo", preco: 69.5},
+	router.Run()
+}
+
+func getPizzas(c *gin.Context) {
+
+	var pizzas = []models.Pizza{
+		{ID: 1, Nome: "Toscana", Preco: 49.5},
+		{ID: 2, Nome: "Marguerita", Preco: 79.5},
+		{ID: 3, Nome: "Atum com queijo", Preco: 69.5},
 	}
 
-	fmt.Println(pizzas)
-	router.Run()
+	c.JSON(200, gin.H{
+		"pizzas": pizzas,
+	})
 }
